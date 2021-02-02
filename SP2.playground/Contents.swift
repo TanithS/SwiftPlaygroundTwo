bc6df015cc23a18d4157818a5c99f8f66e63e9c1
TanithS <77208045+TanithS@users.noreply.github.com>
2021-02-02 10:25:42 -1000

    Completed Parts 1-4; need to complete Part 5
    Class #7 - Tuesday, February 2, 2021: SP2

---
+++ SP2.playground/Contents.swift
@@ -0,0 +1,46 @@
+import UIKit
+
+var str = "Hello, playground"
+
+//SP2 Introduction to If/Else Statements (Part 1)
+//Class #7 - Tuesday, February 2, 2021
+
+//Part 2
+
+var goodGrade = "I've been doing all my work!"
+
+if goodGrade == "I'm not doing very well." //evaluates to "false"
+{
+    //if block
+    print("I should be getting an A at this point.")
+}
+
+//Part 3
+
+else
+{
+    //else block
+    print("I need to work harder.")
+}
+
+//Part 4
+
+var gradeA = true
+
+//create an if/else statement
+
+if gradeA == false
+{
+    print("I really like this class!")
+}
+
+else
+{
+    print("I'm not so sure about coding in Swift...")
+}
+
+//Part 5
+
+
+
+
